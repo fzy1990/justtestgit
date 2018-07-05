638d191adad54f37514583a2bf1172a62bb568a9
fzy1990 <513507038@qq.com>
2018-07-05 10:20:27 +0800

    冯的修改

--- src/main/java/com/hxxn/justtestgit/JusttestgitApplication.java
+++ src/main/java/com/hxxn/justtestgit/JusttestgitApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JusttestgitApplication {
 
     public static void main(String[] args) {
-	    ssssssssssssssssssssssssssss
-		    ssssssssssssss
-		    sssssssssssssssssss
+        System.out.println(11111);
         SpringApplication.run(JusttestgitApplication.class, args);
     }
 }
