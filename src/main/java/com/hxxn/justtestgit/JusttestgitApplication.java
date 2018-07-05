3caeb3f37f9ca6a79023bf3958ac38ea299e14cf
李翼 <yi.li4@chinacreator.com>
2018-07-05 15:06:30 +0800

    2222222222222222

--- src/main/java/com/hxxn/justtestgit/JusttestgitApplication.java
+++ src/main/java/com/hxxn/justtestgit/JusttestgitApplication.java
@@ -11,7 +11,7 @@ public class JusttestgitApplication {
         System.out.println(2222);
         System.out.println(11111);
         System.out.println(11111);
-        System.out.println(11111);
+        System.out.println(2222222222222222222222222);
         SpringApplication.run(JusttestgitApplication.class, args);
     }
 }
