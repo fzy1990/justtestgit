f09a850360e1801b6fd702db294d62142f29f434
fzy1990 <513507038@qq.com>
2018-07-05 10:24:32 +0800

    呵呵

--- src/main/java/com/hxxn/justtestgit/JusttestgitApplication.java
+++ src/main/java/com/hxxn/justtestgit/JusttestgitApplication.java
@@ -8,6 +8,7 @@ public class JusttestgitApplication {
 
     public static void main(String[] args) {
         System.out.println(11111);
+        System.out.println(2222);
         SpringApplication.run(JusttestgitApplication.class, args);
     }
 }
