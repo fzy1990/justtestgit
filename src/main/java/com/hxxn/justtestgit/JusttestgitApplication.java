2125eabe8d8edb3631a37b7c0b82892b6ed60ea8
李翼 <yi.li4@chinacreator.com>
2018-07-05 10:15:23 +0800

    11111111111111111

--- src/main/java/com/hxxn/justtestgit/JusttestgitApplication.java
+++ src/main/java/com/hxxn/justtestgit/JusttestgitApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JusttestgitApplication {
 
     public static void main(String[] args) {
+	    ssssssssssssssssssssssssssss
+		    ssssssssssssss
+		    sssssssssssssssssss
         SpringApplication.run(JusttestgitApplication.class, args);
     }
 }
