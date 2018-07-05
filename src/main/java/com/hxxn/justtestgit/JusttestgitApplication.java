f0117b16e2067547411f902438cd26782e0af57c
fzy1990 <513507038@qq.com>
2018-07-05 10:00:52 +0800

    Initial commit

---
+++ src/main/java/com/hxxn/justtestgit/JusttestgitApplication.java
@@ -0,0 +1,12 @@
+package com.hxxn.justtestgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusttestgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JusttestgitApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hxxn/justtestgit/JusttestgitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hxxn.justtestgit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JusttestgitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
